70bb225577161c7a867934f25e87b5bcddf08c2a
therealproblem <jiahao10594@hotmail.com>
2025-10-01 12:01:13 +0800

    First Commit

---
+++ web/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import { ModeWatcher } from 'mode-watcher';
+	let { children } = $props();
+</script>
+
+<ModeWatcher />
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+{@render children?.()}
