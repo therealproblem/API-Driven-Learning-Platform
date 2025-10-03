1f758ca3f4bcbfa2585f6fcf107208f09bfd891f
therealproblem <jiahao10594@hotmail.com>
2025-10-03 23:17:20 +0800

    configured CORS

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,15 +1,18 @@
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import express from 'express';
 import passport from 'passport';
 import { generateData } from './controllers/dummyController';
 import authMiddleware from './middlewares/authMiddleware';
+import corsOptions from './middlewares/corsMiddleware';
 import errorMiddleware from './middlewares/errorMiddleware';
 import routes from './routes/index';
 
 const app = express();
 
 // Middlewares
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
---
+++ app/src/middlewares/corsMiddleware.ts
@@ -0,0 +1,17 @@
+import cors from 'cors';
+
+const allowedOrigins = ['http://localhost:5173'];
+const corsOptions: cors.CorsOptions = {
+	origin: (origin, callback) => {
+		if (!origin || allowedOrigins.includes(origin)) {
+			callback(null, true); // Allow the request
+		} else {
+			callback(new Error('Not allowed by CORS')); // Block the request
+		}
+	},
+	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
+	allowedHeaders: ['Content-Type', 'Authorization'], // Allowed request headers
+	credentials: true // Allow sending cookies and HTTP authentication credentials
+};
+
+export default corsOptions;
